d6f9e1b74bb52ad66a40ff616f1fc719acbde61c
Aaron Houlihan <AaronRobertHoulihan@gmail.com>
2023-02-08 16:44:40 -0700

    Add express app

---
+++ src/app.js
@@ -0,0 +1,19 @@
+const express = require('express')
+
+const app = express()
+
+const PORT = 8081
+
+app.get('/', async (req, res, next) => {
+  res.status(200).send('Hello World!')
+})
+
+app.listen(PORT, (err) => {
+  if (err) {
+    console.log('Error starting app:')
+    console.log(err)
+    process.exit(1)
+  }
+
+  console.log(`Server started on port ${PORT}`)
+})
