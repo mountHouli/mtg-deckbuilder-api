fbab05f57e0fdc7b9928069dafa905f757a99447
Aaron Houlihan <AaronRobertHoulihan@gmail.com>
2023-02-11 10:40:14 -0700

    DB: Add UserCards table

--- db/setup.sql
+++ db/setup.sql
@@ -11,3 +11,22 @@ CREATE TABLE IF NOT EXISTS Cards (
 CREATE TABLE IF NOT EXISTS Users (
   email VARCHAR(100) primary key
 );
+
+CREATE TABLE IF NOT EXISTS UserCards (
+  userCardId SMALLINT unsigned auto_increment primary key
+    COMMENT 'This is Unsigned SMALLINT because, as of 2023, there are appx 20k Magic cards. Thus, the unsigned smallint max of 65535 is plenty big.',
+  userEmail VARCHAR(100) not null,
+  cardName VARCHAR(150) not null,
+  cardCount TINYINT unsigned not null
+    COMMENT 'Number of copies of this card the user has in their collection.',
+
+  CONSTRAINT `fk__UserCards_userEmail__Users_email`
+    FOREIGN KEY (userEmail) REFERENCES Users (email)
+      ON DELETE CASCADE
+      ON UPDATE CASCADE,
+
+  CONSTRAINT `fk__UserCards_cardName__Cards_name`
+      FOREIGN KEY (cardName) REFERENCES Cards (name)
+      ON DELETE RESTRICT
+      ON UPDATE RESTRICT
+);
