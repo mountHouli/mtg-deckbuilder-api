8ad772de334f4b7186b13b890caceb5cd48f7701
Aaron Houlihan <AaronRobertHoulihan@gmail.com>
2023-02-11 12:56:39 -0700

    DB: In SQL, use leading cammas instead of trailing

--- db/setup.sql
+++ db/setup.sql
@@ -3,8 +3,8 @@ CREATE DATABASE IF NOT EXISTS deckbuilder;
 USE deckbuilder;
 
 CREATE TABLE IF NOT EXISTS Cards (
-  name VARCHAR(150) primary key,
-  scryfallData VARCHAR(50) not null
+  name VARCHAR(150) primary key
+, scryfallData JSON not null
     COMMENT 'Card data from Scryfall.com, in JSON format'
 );
 
@@ -14,18 +14,18 @@ CREATE TABLE IF NOT EXISTS Users (
 
 CREATE TABLE IF NOT EXISTS UserCards (
   userCardId SMALLINT unsigned auto_increment primary key
-    COMMENT 'This is Unsigned SMALLINT because, as of 2023, there are appx 20k Magic cards. Thus, the unsigned smallint max of 65535 is plenty big.',
-  userEmail VARCHAR(100) not null,
-  cardName VARCHAR(150) not null,
-  cardCount TINYINT unsigned not null
-    COMMENT 'Number of copies of this card the user has in their collection.',
+    COMMENT 'This is Unsigned SMALLINT because, as of 2023, there are appx 20k Magic cards. Thus, the unsigned smallint max of 65535 is plenty big.'
+, userEmail VARCHAR(100) not null
+, cardName VARCHAR(150) not null
+, cardCount TINYINT unsigned not null
+    COMMENT 'Number of copies of this card the user has in their collection.'
 
-  CONSTRAINT fk_UserCards_userEmail
+, CONSTRAINT fk_UserCards_userEmail
     FOREIGN KEY (userEmail) REFERENCES Users (email)
       ON DELETE CASCADE
-      ON UPDATE CASCADE,
+      ON UPDATE CASCADE
 
-  CONSTRAINT fk_UserCards_cardName
+, CONSTRAINT fk_UserCards_cardName
       FOREIGN KEY (cardName) REFERENCES Cards (name)
       ON DELETE RESTRICT
       ON UPDATE RESTRICT
